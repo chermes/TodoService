1d1458f98fe383323796c5088ccd8dee27069601
chermes <hermes@hausmilbe.net>
2021-02-24 21:47:57 +0100

    configure api base path via axios config

--- todo_webui/src/main.js
+++ todo_webui/src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Buefy from 'buefy'
-import 'buefy/dist/buefy.css'
+import Vue from 'vue';
+import App from './App.vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
+import Axios from 'axios';
 
-Vue.use(Buefy)
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
+
+Vue.use(Buefy);
 
 new Vue({
   render: h => h(App),
